1d2e6768c88f64c575ee875ddbcb4aaa0522d788
Sandeep Saini <sing1176@algonquinlive.com>
2022-06-10 13:54:04 -0400

    added link to os

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,6 +4,13 @@ const HomePage = () => {
   return (
 		<div className="app text-white bg-gradient-to-b from-gray-800 to-blue-600 min-h-screen">
 			<header className="text-5xl underline pt-2 ">MooseSquad</header>
+			<div className="py-2">
+				<button className="text-xl  border-1 px-2 mt-4 text-black  bg-fuchsia-400 rounded-xl  ">
+					<a target="_blank" href="https://opensea.io/collection/moose-squad">
+						Open Sea
+					</a>
+				</button>
+			</div>
 			<section>
 				<section className="text-3xl m-4">
 					<p>Welcome to the dopest collection of 1111 Squad members</p>
@@ -14,7 +21,7 @@ const HomePage = () => {
 				</section>
 
 				<section className="">
-					<div className="flex justify-center m-2">
+					<div className="flex justify-center pt-4 px-4 pb-20">
 						<iframe
 							src="https://gateway.ipfscdn.io/ipfs/QmfJu3spsSJot6givCK2VjwEgVHymc5RCXHqfG1W5WZyFX/nft-drop.html?contract=0xa18ba65eB2Fddf30158682543500b49E695081D4&chainId=1"
 							width="600px"
