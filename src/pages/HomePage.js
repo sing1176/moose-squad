6946007ab21d0da60bc4c26b0ddd81fd0d2df1ed
Sandeep Saini <sing1176@algonquinlive.com>
2022-06-10 14:31:23 -0400

    added twitter link

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,11 +4,12 @@ const HomePage = () => {
   return (
 		<div className="app text-white bg-gradient-to-b from-gray-800 to-blue-600 min-h-screen">
 			<header className="text-5xl underline pt-2 ">MooseSquad</header>
-			<div className="py-2">
+			<div className="py-2 ">
 				<button className="text-xl  border-1 px-2 mt-4 text-black  bg-fuchsia-400 rounded-xl  ">
-					<a href="https://opensea.io/collection/moose-squad">
-						Open Sea
-					</a>
+					<a href="https://opensea.io/collection/moose-squad">Open Sea</a>
+				</button>
+				<button className="text-xl  border-1 px-2 mt-4 text-black mx-2  bg-fuchsia-400 rounded-xl  ">
+					<a href="https://twitter.com/MooseSquad2022">Twitter</a>
 				</button>
 			</div>
 			<section>
@@ -23,7 +24,7 @@ const HomePage = () => {
 				<section className="">
 					<div className="flex justify-center pt-4 px-4 pb-20">
 						<iframe
-                            title='mint'
+							title="mint"
 							src="https://gateway.ipfscdn.io/ipfs/QmfJu3spsSJot6givCK2VjwEgVHymc5RCXHqfG1W5WZyFX/nft-drop.html?contract=0xa18ba65eB2Fddf30158682543500b49E695081D4&chainId=1"
 							width="600px"
 							height="600px"
